4400db94c5731ba381281f86a4fae0e2ce90e81d
unknown <dipeshhaldar787@gmail.com>
2024-01-21 19:03:43 +0530

    HomePage Updated

--- Frontend/src/pages/Events.jsx
+++ Frontend/src/pages/Events.jsx
@@ -2,15 +2,25 @@ import { Fragment } from "react";
 import "./Events.css";
 
 const Events = () => {
-    return (
-        <Fragment>
-            <div className="outer-event-container">
-                <h4 className="heading-event">WELCOME TO TECHNOTSAV 2024</h4>
-                <h1 className="heading2-event">EVENTS</h1>
-                <p className="heading3-event">Experience the thrill and elation of mind blowing events from tech to gaming. Participate, witness and be a part of the phenomenon</p>
-            </div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div className="outer-event-container">
+        <img
+          className="event__backgroundimage"
+          src="/EventsPage/Events 1 1.png"
+          alt="Background"
+        />
+        <div className="event-text-container">
+          <h4 className="heading-event">WELCOME TO TECHNOTSAV 2024</h4>
+          <h1 className="heading2-event">EVENTS</h1>
+          <p className="heading3-event">
+            Experience the thrill and elation of mind-blowing events from tech
+            to gaming. Participate, witness, and be a part of the phenomenon
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Events;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -6,8 +6,10 @@ const Home = () => {
       <div className="background-image">
         <img src="/HomePage/Chill Out Wallpaper.png" alt="Background" />
         <div className="centered-text">
-          <h1>TECHNOUTSAV</h1>
-          <h1>2024</h1>
+          <h1>TECHNOUTSAV</h1><br />
+          <h1 className='Year-2024'>2024</h1>
+          <br />
+          <h2 className='technoutsav__date'>25 - 27 JANUARY</h2>
           <p>
             <img src="/HomePage/Down Button.png" alt="Small Image" />
           </p>
