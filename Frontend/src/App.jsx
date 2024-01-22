36a9deb5aac916506a25edc7ba9a5a27505dd221
unknown <dipeshhaldar787@gmail.com>
2024-01-22 16:39:06 +0530

    HomePage Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,30 +2,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Events from "./pages/Events";
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage";
-=======
 import LeaderBoard from "./pages/LeaderBoard";
 import Cricket from "./components/Flow/Cricket";
->>>>>>> 22346dbc7a6fd6931c8860f46abf8c232a753677
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        
-        <Route path="/events" element={<Events />} />
-      </Routes>
-    </>
-=======
       <>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/events" element={<Events />} />
           <Route path="/leaderBoard" element={<LeaderBoard />} />
 
@@ -34,7 +21,6 @@ function App() {
           <Route path="/event/cricket" element={<Cricket />} />
         </Routes>
       </>
->>>>>>> 22346dbc7a6fd6931c8860f46abf8c232a753677
   );
 }
 
