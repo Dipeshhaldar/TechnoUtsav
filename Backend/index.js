c90dbd3be681faa166cb84aac194659ab7a95f07
harshalhonde21 <harshalhonde17@gmail.com>
2024-01-22 21:31:21 +0530

    setup the backend fully

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+import config from "./config.js";
+
+app.use(cors());
+app.use(express.json());
+
+const mongoURI = config.mongoURI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => app.listen(5500))
+  .then(() => console.log("connected to db at port 5500 :)"))
+  .catch((err) => console.log(`${err} is error`));
+    
